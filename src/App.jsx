578a42a5405f2d08401e2c63ea5259f82be24fa1
specsxc <piotr.dolgoszyja1@gmail.com>
2026-01-14 03:12:10 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main className="container">
+      <div className="card">
+        <h1>Weather app</h1>
+        <button className="primary-btn">test</button>
+      </div>
+    </main>
+  );
+}
